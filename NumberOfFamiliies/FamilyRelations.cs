e20577e2ec0e426f5c8fc58ee7cc7a2342da2271
José Ruben Lecoña Cuevas <jrlecona@gmail.com>
2021-07-09 19:24:03 -0400

    Add the project to find number of families

---
+++ NumberOfFamiliies/FamilyRelations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberOfFamiliies
+{
+    class FamilyRelations
+    {
+        public int VerifyFamilies(List<Tuple<string, string>> relations)
+        {
+            return DistinctFamilies(relations);
+        }
+
+        private int DistinctFamilies(List<Tuple<string, string>> relations)
+        {
+            List<string> currentFamily = new List<string>();
+            List<List<string>> groupedFamilies = new List<List<string>>();
+            foreach (Tuple<string, string> T in relations)
+            {
+                if (currentFamily.Count == 0 || currentFamily.Contains(T.Item1) || currentFamily.Contains(T.Item2))
+                {
+                    currentFamily.Add(T.Item1);
+                    currentFamily.Add(T.Item2);
+                }
+                else
+                {
+                    currentFamily.Sort();
+                    groupedFamilies.Add(currentFamily.Distinct().ToList());
+                    currentFamily = new List<string>();
+                    currentFamily.Add(T.Item1);
+                    currentFamily.Add(T.Item2);
+                }
+            }
+            if (currentFamily.Count > 0)
+            {
+                currentFamily.Sort();
+                groupedFamilies.Add(currentFamily.Distinct().ToList());
+            }
+
+            return groupedFamilies.Count;
+        }
+    }
+}
---
+++ NumberOfFamiliies/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfFamiliies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You're given a list of pairs that represent direct relationships between 2 persons. Each pair means that these persons are directly related. For example:");
+            Console.WriteLine("  'john', 'jane'");
+            Console.WriteLine("  'john', 'peter'");
+            Console.WriteLine("  'john', 'samuel'");
+            Console.WriteLine("  'peter', 'mark'");
+            Console.WriteLine("  'mark', 'sasha'");
+            Console.WriteLine("  'kirk', 'tom'");
+            Console.WriteLine("  'tom', 'erica'");
+
+            Console.WriteLine(" ");
+
+            var tupleList = new List<Tuple<string, string>>
+              {
+                  Tuple.Create("A", "B"),
+                  Tuple.Create("A", "C"),
+                  Tuple.Create("A", "D"),
+                  Tuple.Create("C", "E"),
+                  Tuple.Create("F", "G"),
+                  Tuple.Create("G", "H"),
+                  Tuple.Create("I", "J"),
+                  Tuple.Create("J", "K")
+              };
+
+            FamilyRelations obj = new FamilyRelations();
+
+            var numberFamilies = obj.VerifyFamilies(tupleList);
+
+
+            Console.WriteLine("Number of Families = {0}", numberFamilies);
+        }
+
+        
+
+    }
+}
